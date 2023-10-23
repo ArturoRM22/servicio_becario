e9d947234c9a16604e478d93e9f41395b2e9853a
ArturoRM22 <A01643269@tec.mx>
2023-10-23 15:15:14 -0600

    incluyendo cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 import rutas from './routes/salones.routes.js';
+
 
 const app = express();
 
+//middlewares
+app.use(cors());
 app.use(express.json());
+
+
 
 app.use('/api',rutas);
 
